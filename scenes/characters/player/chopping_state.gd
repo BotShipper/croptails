b7e05e8f8a3dfdd3d51b73751c6c6b1fa5d39708
MinhQuan <quanaptx4869@gmail.com>
2025-07-03 22:56:01 +0700

    Grow crops

--- scenes/characters/player/chopping_state.gd
+++ scenes/characters/player/chopping_state.gd
@@ -4,6 +4,7 @@ extends NodeState
 @export var animated_sprite_2d: AnimatedSprite2D
 @export var hit_component_colllision_shape: CollisionShape2D
 
+
 func _ready() -> void:
 	hit_component_colllision_shape.disabled = true
 	hit_component_colllision_shape.position = Vector2(0, 0)
